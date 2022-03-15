a5119b22f286d3c4793acdd7349a4104f60881b4
Hyper2109 <darkscorpion40@gmail.com>
2022-03-15 20:16:13 +0100

    update

--- script.js
+++ script.js
@@ -135,3 +135,35 @@ function p4(){
     document.getElementById("p3").classList.remove("s-active");
     document.getElementById("p4").classList.add("s-active");
 }
+
+
+function bt1(){
+    document.getElementById("t-title").innerHTML = "LAUNCH VEHICLE";
+    document.getElementById("t-description").innerHTML = "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!";
+
+    document.getElementById("t-img").src = "assets/technology/image-launch-vehicle-portrait.jpg";
+
+    document.getElementById("b1").classList.add("b-active");
+    document.getElementById("b2").classList.remove("b-active");
+    document.getElementById("b3").classList.remove("b-active");
+}
+function bt2(){
+    document.getElementById("t-title").innerHTML = "SPACEPORT";
+    document.getElementById("t-description").innerHTML = "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.";
+
+    document.getElementById("t-img").src = "assets/technology/image-spaceport-portrait.jpg";
+
+    document.getElementById("b1").classList.remove("b-active");
+    document.getElementById("b2").classList.add("b-active");
+    document.getElementById("b3").classList.remove("b-active");
+}
+function bt3(){
+    document.getElementById("t-title").innerHTML = "SPACE CAPSULE";
+    document.getElementById("t-description").innerHTML = "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.";
+
+    document.getElementById("t-img").src = "assets/technology/image-space-capsule-portrait.jpg";
+
+    document.getElementById("b1").classList.remove("b-active");
+    document.getElementById("b2").classList.remove("b-active");
+    document.getElementById("b3").classList.add("b-active");
+}
